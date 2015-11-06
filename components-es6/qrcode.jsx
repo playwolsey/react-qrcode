fcc97fe27da928709cd599fd6c09148f261398a7
cp <chengpengcp9@163.com>
2015-11-06 17:40:29 +0800

    qr init

---
+++ components-es6/qrcode.jsx
@@ -0,0 +1,26 @@
+/**
+ * @desc qrcode components jsx
+ *
+ * @fileoverview
+ * - this library has no dependencies.
+ * - created on 2015-09-21
+ *
+ * @author awwwesssooooome <chengpengcp9@gmail.com>
+ * @version 0.0.1
+ * @see <a href="http://playwolsey.github.com/xxxx/" target="_blank"></a>
+ */
+
+
+'use strice';
+
+import React, { Component } from 'react/addons';
+
+class QRcode extends Component{
+    render() {
+        return (
+            <a ref="qrcode"></a>
+        )
+    }
+};
+
+export default QRcode;
---
+++ components/qrcode.jsx
@@ -0,0 +1,7 @@
+var Backtop = React.createClass({
+    render: function() {
+        return (
+            null;
+        )
+    }
+});
